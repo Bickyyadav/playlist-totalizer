53af2ae4e01c4b44c7039565fc898189473286d4
Vicky Yadav <ybicky86@gmail.com>
2025-08-13 00:29:49 +0530

    updated index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,15 +9,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+app.use(cors({ origin: "*" }));
 
 app.use(express.json());
 app.use(bodyParser.json());
